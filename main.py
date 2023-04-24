0c461379c0394d2cac41384c555e46438942a3db
Idrissa <idrisabkar2018@gmail.com>
2023-04-24 18:01:50 +0300

    final touches

--- main.py
+++ main.py
@@ -17,5 +17,13 @@ async def home(body: Request):
         scraper = HTMLScraper(url, i)
         el = scraper.find_elements(selector=methode, value=value)
         elements.extend(el)
-        if el[0] == "Invalid Url!": break
-    return {"response": elements}
+        if el[0] == "Invalid Url!":
+            return HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid Url")
+    return {
+        "code": status.HTTP_200_OK,
+        "selector": methode,
+        "value": value,
+        "data": elements
+    }
